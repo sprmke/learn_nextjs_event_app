d09114db5d36615559119aab923d570547d53464
Michael Manlulu <michaeldmanlulu@gmail.com>
2022-10-12 08:01:17 +0800

    79. working on the "All Events" page

--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -1,5 +1,15 @@
+import EventList from '../../components/events/event-list';
+import { getAllEvents } from '../../dummy-data';
+import { Event } from '../../types';
+
 const AllEventsPage = () => {
-  return <div>AllEventsPage</div>;
+  const events: Event[] = getAllEvents();
+
+  return (
+    <div>
+      <EventList items={events} />
+    </div>
+  );
 };
 
 export default AllEventsPage;
