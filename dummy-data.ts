9f7b8b21e0227b597450c51d871ae2eb12194e87
Michael Manlulu <michaeldmanlulu@gmail.com>
2022-10-12 09:29:02 +0800

    fix deployment issues

--- dummy-data.ts
+++ dummy-data.ts
@@ -55,5 +55,5 @@ export function getFilteredEvents(dateFilter: { year: number; month: number }) {
 }
 
 export function getEventById(id: string) {
-  return DUMMY_EVENTS.find((event) => event.id === id);
+  return DUMMY_EVENTS.find((event) => event.id === id) ?? {};
 }
