7d9c91a3b17ebdaecd3329a2774239f6a2f8a72f
Michael Manlulu <michaeldmanlulu@gmail.com>
2022-10-18 16:59:35 +0800

    121. working on the "all events" page

--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -1,11 +1,13 @@
+import { GetStaticProps, InferGetStaticPropsType } from 'next';
 import { useRouter } from 'next/router';
 import EventList from '../../components/events/event-list';
 import EventSearch from '../../components/events/event-search';
-import { getAllEvents } from '../../dummy-data';
+import { getAllEvents } from '../../helpers/api-util';
 import { Event } from '../../types';
 
-const AllEventsPage = () => {
-  const events: Event[] = getAllEvents();
+const AllEventsPage = ({
+  events,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
   const router = useRouter();
 
   const findEventsHandler = (year: string, month: string) => {
@@ -23,3 +25,14 @@ const AllEventsPage = () => {
 };
 
 export default AllEventsPage;
+
+export const getStaticProps: GetStaticProps = async () => {
+  const allEvents: Event[] = await getAllEvents();
+
+  return {
+    props: {
+      events: allEvents,
+    },
+    revalidate: 60,
+  };
+};
