a0ace782af7f269a4070449e04578bae8698f519
Michael Manlulu <michaeldmanlulu@gmail.com>
2022-10-19 10:09:13 +0800

    128. configuring the head content

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Learn NextJS: Event App (Pages & File-based Routing)
+# Learn NextJS: Events App
 
 ##### Course:
 
@@ -6,5 +6,5 @@
 
 ##### Application Scope:
 
-- S4-&-6: Event App (Pages & File-based Routing, Pre-rendering & Data Fetching)
-- Topics discussed: Apply NextJS Pages & File-based Routing, Pre-rendering & Data Fetching Topics
+- S4,6-7: Events App
+- Topics discussed: Apply NextJS Pages & File-based Routing, Pre-rendering & Data Fetching, Optimizing NextJS Apps Topics
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,20 @@
 import { GetStaticProps, InferGetStaticPropsType } from 'next';
 import EventList from '../components/events/event-list';
 import { getFeaturedEvents } from '../helpers/api-util';
+import Head from 'next/head';
 
 const HomePage = ({
   events,
 }: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
     <div>
+      <Head>
+        <title>NextJS Events</title>
+        <meta
+          name='description'
+          content='Find a lot of great events that allow you to evolve...'
+        />
+      </Head>
       <EventList items={events} />
     </div>
   );
